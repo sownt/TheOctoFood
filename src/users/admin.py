bc017defcc6062e36911ff5425ed0f1abd093ee0
sownt <thonsai@outlook.com>
2022-01-03 13:35:45 +0700

    Add account model
     Features:
     - Add account model
     - Add CustomModelManager
     - Add CustomAdmin

--- src/users/admin.py
+++ src/users/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Account
 
 # Register your models here.
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'phone_number', 'date_joined', 'is_active', 'is_staff', 'is_superuser',)
+    list_display_links = ('username',)
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+admin.site.register(Account, CustomUserAdmin)
