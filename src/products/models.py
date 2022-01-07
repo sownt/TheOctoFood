37bda34d078a175393c16a1bc5442c1e32615fe3
doanhtuyettt <doanhtuyet_t64@hus.edu.vn>
2022-01-07 21:31:28 +0700

    Add models.py

--- src/products/models.py
+++ src/products/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
-    name        = models.CharField(max_length=50)
-    slug        = models.SlugField(max_length=50)
+    name        = models.CharField(max_length=50, unique=True)
+    slug        = models.SlugField(max_length=50, unique=True)
     description = models.TextField()
     image       = models.ImageField(upload_to ='categories/')
