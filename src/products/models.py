e6fa9c04ce1d5354ccf17125beee1c3a5b4c14c4
doanhtuyettt <doanhtuyet_t64@hus.edu.vn>
2022-01-07 22:06:17 +0700

    Add models

--- src/products/models.py
+++ src/products/models.py
@@ -8,4 +8,10 @@ class Category(models.Model):
     image       = models.ImageField(upload_to ='categories/')
 
 class Item(models.Model):
-    pass
+    name        = models.CharField(max_length=50, unique=True)
+    slug        = models.SlugField(max_length=50, unique=True)
+    description = models.TextField()
+    price       = models.DecimalField(max_digits=500000, decimal_places=2)
+    image       = models.ImageField(upload_to ='categories/')
+    quantity    = models.DecimalField(max_digits=1000, decimal_places=2)
+pass
