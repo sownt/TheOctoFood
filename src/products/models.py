881df4a26a951b01fe979d640ca9b9dfe2fdda84
doanhtuyettt <doanhtuyet_t64@hus.edu.vn>
2022-01-09 15:11:42 +0700

    Add models

--- src/products/models.py
+++ src/products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
 class Category(models.Model):
@@ -15,3 +16,9 @@ class Item(models.Model):
     image       = models.ImageField(upload_to ='items/')
     category    = models.ForeignKey(Category, on_delete = models.CASCADE) 
     stock       = models.IntegerField()
+
+class Review(models.Model):
+    image       = models.ImageField(upload_to ='reviews/')
+    comment     = models.TextField(max_length= 200)
+    item        = models.ForeignKey(Item, on_delete= models.CASCADE)
+    slug        = models.SlugField(max_length=50, unique=True)
